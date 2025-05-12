76046d7aacd0f6e8e1e721af8fd3d4e6b1d9e0c4
FelipePhipe <felipeparragranda@gmail.com>
2025-05-11 20:27:46 -0500

    nose si funca o no

--- app.py
+++ app.py
@@ -1,47 +1,55 @@
 from benchmarking import Benchmarking
 from metodos_ordenamiento import MetodosOrdenamiento
 import matplotlib.pyplot as plt
-import time
 from datetime import datetime
 
 if __name__ == "__main__":
-
-
     bench = Benchmarking()
     metodosO = MetodosOrdenamiento()
 
-    tamanios = [5000, 10000, 30000,50000,100000]
+    tamanios = [5000, 10000, 30000, 50000, 100000]
     resultados = []
 
     metodos_dic = {
-        "burbuja": metodosO.metodo_sort_bubble,
-        "burbuja_mejorado": metodosO.metodo_sort_bubble_mejorado,
+        "bubble": metodosO.metodo_sort_bubble,
+        "bubbleMejorado": metodosO.metodo_sort_bubble_mejorado,
         "seleccion": metodosO.metodo_sort_seleccion,
+        "insercion": metodosO.metodo_sort_insercion,
         "shell": sorted
     }
 
+    arreglo_completo = bench.build_arreglo(100000)
+    subconjuntos = {
+        5000: arreglo_completo[:5000],
+        10000: arreglo_completo[:10000],
+        30000: arreglo_completo[:30000],
+        50000: arreglo_completo[:50000],
+        100000: arreglo_completo
+    }
+
     for tam in tamanios:
-        arreglo_base = bench.build_arreglo(tam)
-        for nombre, metodo in metodos_dic.items():
-            tiempo = bench.medir_tiempo(metodo, arreglo_base)
-            resultados.append((tam, nombre, tiempo))
+        arreglo_original = subconjuntos[tam]
+    for nombre, metodo in metodos_dic.items():
+        arreglo_clonado = arreglo_original.copy()  
+        tiempo = bench.medir_tiempo(metodo, arreglo_clonado)
+        resultados.append((tam, nombre, tiempo))
+        print(f"Tamaño: {tam}, Algoritmo: {nombre}, Tiempo: {tiempo:.4f} segundos")
+
 
     tiempos_by_metodo = {nombre: [] for nombre in metodos_dic}
     for tam, nombre, tiempo in resultados:
         tiempos_by_metodo[nombre].append(tiempo)
 
-  
     plt.figure(figsize=(12, 6))
-
     now = datetime.now()
-    plt.suptitle(f"FELIPE PARRA - {now.strftime('%d/%m/%Y %H:%M:%S')}", fontsize=12, weight='bold')
+    plt.suptitle(f"FELIPE PARRA -WELLINGTON GUZMÁN - {now.strftime('%d/%m/%Y %H:%M:%S')}", fontsize=12, weight='bold')
 
-  
     ax1 = plt.subplot(1, 2, 1)
     colores = {
-        "burbuja": "blue",
-        "burbuja_mejorado": "orange",
+        "bubble": "blue",
+        "bubbleMejorado": "orange",
         "seleccion": "green",
+        "insercion": "purple",
         "shell": "red"
     }
 
@@ -54,7 +62,6 @@ if __name__ == "__main__":
     ax1.grid(True)
     ax1.legend()
 
- 
     ax2 = plt.subplot(1, 2, 2)
     x = [1, 2, 3, 4, 5]
     y = [2, 4, 6, 8, 10]
